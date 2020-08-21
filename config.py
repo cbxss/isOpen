aeaea6b7f9340e9bfd017f8b98d06c01a117055e
cbass <44591891+cbass5@users.noreply.github.com>
2020-08-20 18:48:20 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-account_sid = ""
-auth_token = ""
-username = ""
-password = ""
-myNumber = ''
-twilioNumber = ''
+account_sid = "" #from twilio
+auth_token = "" #from twilio
+username = "" #csulb ID number
+password = "" #csulb password
+myNumber = ''  #your phone number
+twilioNumber = '' #twilio phone number
 courseNumber = "" #examples: 174, 100
 subject = ''  #examples: CECS, MAE
